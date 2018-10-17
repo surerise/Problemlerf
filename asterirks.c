74791205ee160a46304eb3253872f53ecb212c88
sheldorion <oguzhanonal52@hotmail.com>
2018-10-17 23:30:08 +0300

    Create asterirks.c

---
+++ asterirks.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int sayac=0;
+
+
+    while(sayac<100) {
+
+    if(sayac%10==0) {printf("\n");}
+
+    printf("*");
+
+    sayac++;
+
+    }
+return 0;
+
+}
