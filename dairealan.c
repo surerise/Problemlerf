11c82e9318ac94da9b20b15583abe29962df7aff
sheldorion <oguzhanonal52@hotmail.com>
2018-10-17 23:46:38 +0300

    Create dairealan.c

---
+++ dairealan.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+ float pi=3.14159;
+ float yari;
+
+ printf("Yaricapi girin:");
+ scanf("%f",&yari);
+ float alan= yari*yari*pi;
+ printf("Alan:%f",alan);
+
+
+
+
+
+
+
+return 0;
+
+}
