9b44b6b3a377558b50068e99bcbf8a07279d3530
sheldorion <oguzhanonal52@hotmail.com>
2018-10-19 13:32:51 +0300

    Update sifreleme.c

--- sifreleme.c
+++ sifreleme.c
@@ -3,7 +3,8 @@
 
 int main() {
 
-int sayi,dort,uc,iki,ilk,yenisayi;
+int sayi,dort,uc,iki,yenisayi;
+int ilk;
 
 //sayıyı alma
 printf("4 basamakli bir sayi girin: ");
@@ -13,17 +14,21 @@ scanf("%d",&sayi);
 //şifreleme kısmı
 dort=sayi%10; sayi=sayi/10;
 
-uc=sayi%10; sayi=sayi/10; uc=(uc+7)/10;
+uc=sayi%10; sayi=sayi/10;
 
-iki=sayi%10; sayi=sayi/10; iki=(iki+7)/10;
+iki=sayi%10; sayi=sayi/10;
 
-ilk=sayi; ilk=(ilk+7)/10;
+ilk=sayi;
 
 
+ilk= (ilk*1000+7)/10;
+iki= (iki*100+7)/10;
+uc=(uc*10+7)/10;
+dort=(dort+7)/10;
 //şifrenlenmiş sayıyı çıktı alma
-yenisayi=uc*1000+dort*100+ilk*10+iki;
 
-printf("Sifrelenmis veri: %d",yenisayi);
+printf("Sifre:%d%d%d%d",uc,dort,ilk,iki);
 
 return 0;
 }
+
